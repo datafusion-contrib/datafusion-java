56e61ef4e75feb33fcc2f42b590dbe2c6f88b127
Jiayu Liu <jiayu.liu@airbnb.com>
2021-10-14 20:39:52 +0800

    spotless apply

--- lib/src/main/java/org/apache/arrow/datafusion/DataFrame.java
+++ lib/src/main/java/org/apache/arrow/datafusion/DataFrame.java
@@ -1,7 +1,11 @@
 package org.apache.arrow.datafusion;
 
+import java.util.concurrent.CompletableFuture;
 import org.apache.arrow.vector.ipc.ArrowReader;
 
 public interface DataFrame {
-     ArrowReader getReader();
+  ArrowReader getReader();
+
+  /** Print results. */
+  CompletableFuture<Void> show();
 }
