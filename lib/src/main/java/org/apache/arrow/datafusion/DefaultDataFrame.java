7ec8997f79b1e989db93c2a3a7d0b471ac1f5418
Jiayu Liu <jiayu.liu@airbnb.com>
2021-10-16 23:31:46 +0800

    arrow reader done

--- lib/src/main/java/org/apache/arrow/datafusion/DefaultDataFrame.java
+++ lib/src/main/java/org/apache/arrow/datafusion/DefaultDataFrame.java
@@ -1,9 +1,10 @@
 package org.apache.arrow.datafusion;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import org.apache.arrow.memory.RootAllocator;
+import org.apache.arrow.vector.Float8Vector;
+import org.apache.arrow.vector.VectorSchemaRoot;
 import org.apache.arrow.vector.ipc.ArrowFileReader;
 import org.apache.arrow.vector.ipc.message.ArrowRecordBatch;
 import org.apache.arrow.vector.types.pojo.Schema;
@@ -35,22 +36,33 @@ class DefaultDataFrame extends AbstractProxy implements DataFrame {
             result.completeExceptionally(new RuntimeException(errString));
           } else {
             logger.info("successfully completed with arr length={}", arr.length);
-            logger.info("begin to decode");
-            try (RootAllocator allocator = new RootAllocator(Long.MAX_VALUE);
+            try (RootAllocator allocator = new RootAllocator();
                 ByteArrayReadableSeekableByteChannel byteChannel =
                     new ByteArrayReadableSeekableByteChannel(arr);
                 ArrowFileReader reader = new ArrowFileReader(byteChannel, allocator)) {
-              logger.info("reader {}", reader);
-              Schema schema = reader.getVectorSchemaRoot().getSchema();
+              logger.info("before reader loading {}", reader);
+              try {
+                //                boolean loaded = reader.loadNextBatch();
+                //                logger.info("reader {} loaded {}", reader, loaded);
+              } catch (Throwable e) {
+                //                System.out.printf("%s", e.getStackTrace());
+                throw e;
+              }
+              VectorSchemaRoot root = reader.getVectorSchemaRoot();
+              Schema schema = root.getSchema();
               logger.info("schema {}", schema);
-            } catch (IOException e) {
-              logger.warn("failed to read", e);
+              Float8Vector vector = (Float8Vector) root.getVector(0);
+              logger.info(
+                  "vector size {}, row count {}", vector.getValueCount(), root.getRowCount());
+              for (int i = 0; i < root.getRowCount(); i += 1) {
+                logger.info("value {}={}", i, vector.getValueAsDouble(i));
+              }
             } catch (Exception e) {
-              System.out.println(e);
-              logger.warn("unexpected exception", e);
+              logger.warn("failed to read", e);
             }
             result.complete(null);
           }
+          logger.info("returning null");
           return /*void*/ null;
         });
     return result;
