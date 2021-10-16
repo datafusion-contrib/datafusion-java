d75a518e6303283d3ed6ef00d4de5342582212cd
Jiayu Liu <jiayu.liu@airbnb.com>
2021-10-16 21:27:43 +0800

    byte array passing done

--- examples/src/main/java/org/apache/arrow/datafusion/examples/ExampleMain.java
+++ examples/src/main/java/org/apache/arrow/datafusion/examples/ExampleMain.java
@@ -3,9 +3,14 @@ package org.apache.arrow.datafusion.examples;
 import org.apache.arrow.datafusion.DataFrame;
 import org.apache.arrow.datafusion.ExecutionContext;
 import org.apache.arrow.datafusion.ExecutionContexts;
+import org.apache.arrow.vector.ipc.message.ArrowRecordBatch;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class ExampleMain {
 
@@ -23,6 +28,7 @@ public class ExampleMain {
       logger.printf(Level.INFO, "successfully loaded data frame %s", dataFrame);
 
       dataFrame.show().thenRun(() -> logger.info("show was run successfully"));
+      dataFrame.collect().thenAccept(arrowRecordBatches -> logger.info("arrow result {}", arrowRecordBatches));
     }
   }
 }
--- lib/src/main/java/org/apache/arrow/datafusion/DataFrame.java
+++ lib/src/main/java/org/apache/arrow/datafusion/DataFrame.java
@@ -1,10 +1,12 @@
 package org.apache.arrow.datafusion;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import org.apache.arrow.vector.ipc.ArrowReader;
+import org.apache.arrow.vector.ipc.message.ArrowRecordBatch;
 
 public interface DataFrame extends NativeProxy {
-  ArrowReader getReader();
+  /** Collect dataframe into a list of record batches */
+  CompletableFuture<List<ArrowRecordBatch>> collect();
 
   /** Print results. */
   CompletableFuture<Void> show();
