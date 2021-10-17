994cab3f47b1ed2649bdae881079b924cb4653e4
Jiayu Liu <jiayu.liu@airbnb.com>
2021-10-17 08:15:38 +0800

    roll back to 1.8

--- lib/src/main/java/org/apache/arrow/datafusion/DefaultDataFrame.java
+++ lib/src/main/java/org/apache/arrow/datafusion/DefaultDataFrame.java
@@ -32,7 +32,7 @@ class DefaultDataFrame extends AbstractProxy implements DataFrame {
         runtimePointer,
         dataframe,
         (String errString, byte[] arr) -> {
-          if (errString != null && !"".equals(errString)) {
+          if (containsError(errString)) {
             result.completeExceptionally(new RuntimeException(errString));
           } else {
             logger.info("successfully completed with arr length={}", arr.length);
@@ -40,22 +40,17 @@ class DefaultDataFrame extends AbstractProxy implements DataFrame {
                 ByteArrayReadableSeekableByteChannel byteChannel =
                     new ByteArrayReadableSeekableByteChannel(arr);
                 ArrowFileReader reader = new ArrowFileReader(byteChannel, allocator)) {
-              logger.info("before reader loading {}", reader);
-              try {
-                //                boolean loaded = reader.loadNextBatch();
-                //                logger.info("reader {} loaded {}", reader, loaded);
-              } catch (Throwable e) {
-                //                System.out.printf("%s", e.getStackTrace());
-                throw e;
-              }
               VectorSchemaRoot root = reader.getVectorSchemaRoot();
               Schema schema = root.getSchema();
-              logger.info("schema {}", schema);
-              Float8Vector vector = (Float8Vector) root.getVector(0);
-              logger.info(
-                  "vector size {}, row count {}", vector.getValueCount(), root.getRowCount());
-              for (int i = 0; i < root.getRowCount(); i += 1) {
-                logger.info("value {}={}", i, vector.getValueAsDouble(i));
+              logger.info("schema {}, bytes read={}", schema, reader.bytesRead());
+              while (reader.loadNextBatch()) {
+                logger.info("loading next batch");
+                Float8Vector vector = (Float8Vector) root.getVector(0);
+                logger.info(
+                    "vector size {}, row count {}", vector.getValueCount(), root.getRowCount());
+                for (int i = 0; i < root.getRowCount(); i += 1) {
+                  logger.info("value {}={}", i, vector.getValueAsDouble(i));
+                }
               }
             } catch (Exception e) {
               logger.warn("failed to read", e);
@@ -68,6 +63,10 @@ class DefaultDataFrame extends AbstractProxy implements DataFrame {
     return result;
   }
 
+  private boolean containsError(String errString) {
+    return errString != null && !"".equals(errString);
+  }
+
   @Override
   public CompletableFuture<Void> show() {
     Runtime runtime = context.getRuntime();
@@ -78,7 +77,7 @@ class DefaultDataFrame extends AbstractProxy implements DataFrame {
         runtimePointer,
         dataframe,
         (String errString) -> {
-          if (errString != null && !"".equals(errString)) {
+          if (containsError(errString)) {
             future.completeExceptionally(new RuntimeException(errString));
           } else {
             future.complete(null);
