5d9a7c81ed586590345ead8c84eb12995ed3a34b
Jiayu Liu <jiayu.liu@airbnb.com>
2021-10-25 18:25:56 +0800

    split up rust code

---
+++ datafusion_jni/src/dataframe.rs
@@ -0,0 +1,91 @@
+use arrow::ipc::writer::FileWriter;
+use datafusion::dataframe::DataFrame;
+use jni::objects::{JClass, JObject, JValue};
+use jni::sys::jlong;
+use jni::JNIEnv;
+use std::convert::Into;
+use std::io::BufWriter;
+use std::io::Cursor;
+use std::sync::Arc;
+use tokio::runtime::Runtime;
+
+#[no_mangle]
+pub extern "system" fn Java_org_apache_arrow_datafusion_DataFrames_collectDataframe(
+    env: JNIEnv,
+    _class: JClass,
+    runtime: jlong,
+    dataframe: jlong,
+    callback: JObject,
+) {
+    let runtime = unsafe { &mut *(runtime as *mut Runtime) };
+    let dataframe = unsafe { &mut *(dataframe as *mut Arc<dyn DataFrame>) };
+    let schema = dataframe.schema().into();
+    runtime.block_on(async {
+        let batches = dataframe
+            .collect()
+            .await
+            .expect("failed to collect dataframe");
+        let mut buff = Cursor::new(vec![0; 0]);
+        {
+            let mut writer = FileWriter::try_new(BufWriter::new(&mut buff), &schema)
+                .expect("failed to create writer");
+            for batch in batches {
+                writer.write(&batch).expect("failed to write batch");
+            }
+            writer.finish().expect("failed to finish");
+        }
+        let err_message = env
+            .new_string("".to_string())
+            .expect("Couldn't create java string!");
+        let ba = env
+            .byte_array_from_slice(&buff.get_ref())
+            .expect("cannot create empty byte array");
+        env.call_method(
+            callback,
+            "accept",
+            "(Ljava/lang/Object;Ljava/lang/Object;)V",
+            &[err_message.into(), ba.into()],
+        )
+        .expect("failed to call method");
+    });
+}
+
+#[no_mangle]
+pub extern "system" fn Java_org_apache_arrow_datafusion_DataFrames_showDataframe(
+    env: JNIEnv,
+    _class: JClass,
+    runtime: jlong,
+    dataframe: jlong,
+    callback: JObject,
+) {
+    let runtime = unsafe { &mut *(runtime as *mut Runtime) };
+    let dataframe = unsafe { &mut *(dataframe as *mut Arc<dyn DataFrame>) };
+    runtime.block_on(async {
+        let r = dataframe.show().await;
+        let err_message: JValue = match r {
+            Ok(_) => JValue::Void,
+            Err(err) => {
+                let err_message = env
+                    .new_string(err.to_string())
+                    .expect("Couldn't create java string!");
+                err_message.into()
+            }
+        };
+        env.call_method(
+            callback,
+            "accept",
+            "(Ljava/lang/Object;)V",
+            &[err_message.into()],
+        )
+        .expect("failed to call method");
+    });
+}
+
+#[no_mangle]
+pub extern "system" fn Java_org_apache_arrow_datafusion_DataFrames_destroyDataFrame(
+    _env: JNIEnv,
+    _class: JClass,
+    pointer: jlong,
+) {
+    let _ = unsafe { Box::from_raw(pointer as *mut Arc<dyn DataFrame>) };
+}
---
+++ datafusion_jni/src/runtime.rs
@@ -0,0 +1,29 @@
+use jni::objects::JClass;
+use jni::sys::jlong;
+use jni::JNIEnv;
+use std::time::Duration;
+use tokio::runtime::Runtime;
+
+#[no_mangle]
+pub extern "system" fn Java_org_apache_arrow_datafusion_TokioRuntime_createTokioRuntime(
+    _env: JNIEnv,
+    _class: JClass,
+) -> jlong {
+    if let Ok(runtime) = Runtime::new() {
+        println!("successfully created tokio runtime");
+        Box::into_raw(Box::new(runtime)) as jlong
+    } else {
+        // TODO error handling
+        -1
+    }
+}
+#[no_mangle]
+pub extern "system" fn Java_org_apache_arrow_datafusion_TokioRuntime_destroyTokioRuntime(
+    _env: JNIEnv,
+    _class: JClass,
+    pointer: jlong,
+) {
+    let runtime = unsafe { Box::from_raw(pointer as *mut Runtime) };
+    runtime.shutdown_timeout(Duration::from_millis(100));
+    println!("successfully shutdown tokio runtime");
+}
