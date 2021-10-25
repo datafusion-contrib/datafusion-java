365c6855cf80d685ba0cedc2fdad0d2b809d1e4c
Jiayu Liu <jiayu.liu@airbnb.com>
2021-10-25 18:20:02 +0800

    apply async upgrades

--- datafusion_jni/src/lib.rs
+++ datafusion_jni/src/lib.rs
@@ -1,7 +1,7 @@
 use arrow::ipc::writer::FileWriter;
 use datafusion::dataframe::DataFrame;
+use datafusion::error::DataFusionError;
 use datafusion::execution::context::{ExecutionConfig, ExecutionContext};
-use datafusion::physical_plan::csv::CsvReadOptions;
 use datafusion::prelude::*;
 use jni::objects::{GlobalRef, JClass, JObject, JString, JValue};
 use jni::sys::{jbyteArray, jint, jlong, jstring};
@@ -42,12 +42,13 @@ pub extern "system" fn Java_org_apache_arrow_datafusion_TokioRuntime_destroyToki
 pub extern "system" fn Java_org_apache_arrow_datafusion_DefaultExecutionContext_registerParquet(
     env: JNIEnv,
     _class: JClass,
-    _runtime: jlong,
+    runtime: jlong,
     pointer: jlong,
     name: JString,
     path: JString,
     callback: JObject,
 ) {
+    let runtime = unsafe { &mut *(runtime as *mut Runtime) };
     let name: String = env
         .get_string(name)
         .expect("Couldn't get name as string!")
@@ -57,30 +58,33 @@ pub extern "system" fn Java_org_apache_arrow_datafusion_DefaultExecutionContext_
         .expect("Couldn't get name as string!")
         .into();
     let context = unsafe { &mut *(pointer as *mut ExecutionContext) };
-    let register_result = context.register_parquet(&name, &path);
-    let err_message: JValue = match register_result {
-        Ok(_) => JValue::Void,
-        Err(err) => {
-            let err_message = env
-                .new_string(err.to_string())
-                .expect("Couldn't create java string!");
-            err_message.into()
-        }
-    };
-    env.call_method(callback, "accept", "(Ljava/lang/Object;)V", &[err_message])
-        .expect("failed to callback method");
+    runtime.block_on(async {
+        let register_result = context.register_parquet(&name, &path).await;
+        let err_message: JValue = match register_result {
+            Ok(_) => JValue::Void,
+            Err(err) => {
+                let err_message = env
+                    .new_string(err.to_string())
+                    .expect("Couldn't create java string!");
+                err_message.into()
+            }
+        };
+        env.call_method(callback, "accept", "(Ljava/lang/Object;)V", &[err_message])
+            .expect("failed to callback method");
+    });
 }
 
 #[no_mangle]
 pub extern "system" fn Java_org_apache_arrow_datafusion_DefaultExecutionContext_registerCsv(
     env: JNIEnv,
     _class: JClass,
-    _runtime: jlong,
+    runtime: jlong,
     pointer: jlong,
     name: JString,
     path: JString,
     callback: JObject,
 ) {
+    let runtime = unsafe { &mut *(runtime as *mut Runtime) };
     let name: String = env
         .get_string(name)
         .expect("Couldn't get name as string!")
@@ -90,59 +94,66 @@ pub extern "system" fn Java_org_apache_arrow_datafusion_DefaultExecutionContext_
         .expect("Couldn't get name as string!")
         .into();
     let context = unsafe { &mut *(pointer as *mut ExecutionContext) };
-    let register_result = context.register_csv(&name, &path, CsvReadOptions::new());
-    let err_message: JValue = match register_result {
-        Ok(_) => JValue::Void,
-        Err(err) => {
-            let err_message = env
-                .new_string(err.to_string())
-                .expect("Couldn't create java string!");
-            err_message.into()
-        }
-    };
-    env.call_method(callback, "accept", "(Ljava/lang/Object;)V", &[err_message])
-        .expect("failed to callback method");
+    runtime.block_on(async {
+        let register_result = context
+            .register_csv(&name, &path, CsvReadOptions::new())
+            .await;
+        let err_message: JValue = match register_result {
+            Ok(_) => JValue::Void,
+            Err(err) => {
+                let err_message = env
+                    .new_string(err.to_string())
+                    .expect("Couldn't create java string!");
+                err_message.into()
+            }
+        };
+        env.call_method(callback, "accept", "(Ljava/lang/Object;)V", &[err_message])
+            .expect("failed to callback method");
+    });
 }
 
 #[no_mangle]
 pub extern "system" fn Java_org_apache_arrow_datafusion_DefaultExecutionContext_querySql(
     env: JNIEnv,
     _class: JClass,
-    _runtime: jlong,
+    runtime: jlong,
     pointer: jlong,
     sql: JString,
     callback: JObject,
 ) {
+    let runtime = unsafe { &mut *(runtime as *mut Runtime) };
     let sql: String = env
         .get_string(sql)
         .expect("Couldn't get sql as string!")
         .into();
     let context = unsafe { &mut *(pointer as *mut ExecutionContext) };
-    let query_result = context.sql(&sql);
-    match query_result {
-        Ok(v) => {
-            let dataframe = Box::into_raw(Box::new(v)) as jlong;
-            env.call_method(
-                callback,
-                "callback",
-                "(Ljava/lang/String;J)V",
-                &[JValue::Void, dataframe.into()],
-            )
+    runtime.block_on(async {
+        let query_result = context.sql(&sql).await;
+        match query_result {
+            Ok(v) => {
+                let dataframe = Box::into_raw(Box::new(v)) as jlong;
+                env.call_method(
+                    callback,
+                    "callback",
+                    "(Ljava/lang/String;J)V",
+                    &[JValue::Void, dataframe.into()],
+                )
+            }
+            Err(err) => {
+                let err_message = env
+                    .new_string(err.to_string())
+                    .expect("Couldn't create java string!");
+                let dataframe = -1 as jlong;
+                env.call_method(
+                    callback,
+                    "callback",
+                    "(Ljava/lang/String;J)V",
+                    &[err_message.into(), dataframe.into()],
+                )
+            }
         }
-        Err(err) => {
-            let err_message = env
-                .new_string(err.to_string())
-                .expect("Couldn't create java string!");
-            let dataframe = -1 as jlong;
-            env.call_method(
-                callback,
-                "callback",
-                "(Ljava/lang/String;J)V",
-                &[err_message.into(), dataframe.into()],
-            )
-        }
-    }
-    .expect("failed to call method");
+        .expect("failed to call method");
+    });
 }
 
 #[no_mangle]
@@ -215,18 +226,23 @@ pub extern "system" fn Java_org_apache_arrow_datafusion_DataFrames_showDataframe
     let runtime = unsafe { &mut *(runtime as *mut Runtime) };
     let dataframe = unsafe { &mut *(dataframe as *mut Arc<dyn DataFrame>) };
     runtime.block_on(async {
-        // TODO this is only added in Datafusion 6.0
-        // dataframe.show().await;
-        let err_message = env
-            .new_string("".to_string())
-            .expect("Couldn't create java string!");
+        let r = dataframe.show().await;
+        let err_message: JValue = match r {
+            Ok(_) => JValue::Void,
+            Err(err) => {
+                let err_message = env
+                    .new_string(err.to_string())
+                    .expect("Couldn't create java string!");
+                err_message.into()
+            }
+        };
         env.call_method(
             callback,
             "accept",
             "(Ljava/lang/Object;)V",
             &[err_message.into()],
         )
-        .unwrap();
+        .expect("failed to call method");
     });
 }
 
