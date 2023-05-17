513d37e475078c139f915a19cfe1f8a8fc41dd1c
Adam Reeve <adam.reeve@gr-oss.io>
2023-05-17 10:28:10 +0100

    Add missing class and method comments to fix warnings

--- datafusion-java/src/main/java/org/apache/arrow/datafusion/SessionContext.java
+++ datafusion-java/src/main/java/org/apache/arrow/datafusion/SessionContext.java
@@ -6,15 +6,36 @@ import java.util.concurrent.CompletableFuture;
 /** A session context holds resources and is the entrance for obtaining {@link DataFrame} */
 public interface SessionContext extends AutoCloseable, NativeProxy {
 
-  /** Obtain the {@link DataFrame} by running the {@code sql} against the datafusion library */
+  /**
+   * Obtain the {@link DataFrame} by running the {@code sql} against the datafusion library
+   *
+   * @param sql The query to execute
+   * @return DataFrame representing the query result
+   */
   CompletableFuture<DataFrame> sql(String sql);
 
-  /** Registering a csv file with the context */
+  /**
+   * Registering a csv file with the context
+   *
+   * @param name The table name to use to refer to the data
+   * @param path Path to the CSV file
+   * @return Future that is completed when the CSV is registered
+   */
   CompletableFuture<Void> registerCsv(String name, Path path);
 
-  /** Registering a parquet file with the context */
+  /**
+   * Registering a parquet file with the context
+   *
+   * @param name The table name to use to refer to the data
+   * @param path Path to the Parquet file
+   * @return Future that is completed when the Parquet file is registered
+   */
   CompletableFuture<Void> registerParquet(String name, Path path);
 
-  /** Get the runtime associated with this context */
+  /**
+   * Get the runtime associated with this context
+   *
+   * @return The context's runtime
+   */
   Runtime getRuntime();
 }
